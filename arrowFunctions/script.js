a0eeae1236f9a4cf60f80d9b5145bad1d6e2513f
JoacoMaurtua <joacomaurtua@gmail.com>
2021-05-21 19:01:28 -0500

    practice arrow functions

---
+++ arrowFunctions/script.js
@@ -0,0 +1,34 @@
+// notación a mano para devolver un objeto
+// NOTA: el primer conjunto de corchetes define el cuerpo de la función
+// ay el segundo conjunto de corchetes son para crear el objeto literal
+const returnObjLonghand = () => {
+  return { 
+      firstName: 'John',
+      lastName: 'Wick'
+  }
+}
+/**
+*  
+* los corchetes se interpretan como la apertura del cuerpo de 
+* la función en lugar de corchetes para crear un objeto literal 
+*/
+//const returnObj = () => { firstName: 'John', lastName: 'Wick' }
+// rodear el retorno implícito entre parentesis resuelve el problema
+const returnObjFixed = () => ({ firstName: 'John', lastName: 'Wick' });
+
+/* La sintaxis elegante no es el único cambio con las funciones de flecha. También heredan el contexto del ámbito primario. Para demostrar esto, creemos una clase de baraja de cartas. */
+
+class Deck {
+  constructor() {
+    const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
+    const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+    const deck = [];
+    for (const suit of suits) {
+      for (const face of faces) {
+        deck.push(this.createCard(suit, face));
+      }
+    }
+    this.deck = deck;
+  }
+}
+
